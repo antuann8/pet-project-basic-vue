81638b73ec3b580e085f33994a9536a6c46d2851
antuann8 <kashirin.antosha@mail.ru>
2023-10-23 15:41:21 +0300

    first-commit

---
+++ new/test-zhil-fond/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Sidebar />
+  </div>
+</template>
+
+<script>
+import Sidebar from "@/components/Sidebar.vue";
+export default {
+  components: {
+    Sidebar,
+  },
+};
+</script>
+
+<!-- <style scoped></style> -->
---
+++ new/test-zhil-fond/src/components/Sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="header__container">
+      <h1 class="header">Жилфонд</h1>
+      <p class="description">Пользователь</p>
+    </div>
+    <div class="search__container">
+      <div class="find">
+        <p class="find__description">Поиск сотрудников</p>
+        <input
+          class="find__input"
+          type="text"
+          placeholder="Введите id или имя"
+        />
+        <p class="find__description">Результаты</p>
+        <span class="find__item">начните поиск</span>
+      </div>
+      <div class="about"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+};
+</script>
+
+<style src="./../assets/styles/Sidebar.scss" scoped></style>
